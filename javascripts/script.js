e5d4bfc5f44132cb83d4d3231ccaf4b24e1e6052
Priya M <143039601+priya2075@users.noreply.github.com>
2023-08-28 23:46:48 +0800

    Create script.js

---
+++ javascripts/script.js
@@ -0,0 +1,20 @@
+
+
+$(function(){
+    // Back-to-top
+    var offset = 220;
+    var duration = 500;
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > offset) {
+            $('.back-to-top').fadeIn(duration);
+
+        } else {
+            $('.back-to-top').fadeOut(duration);}
+    });
+    
+    $('.back-to-top').click(function(event) {event.preventDefault();
+        $('html, body').animate({
+            scrollTop: 0}, duration);
+        return false;});
+
+}); 
