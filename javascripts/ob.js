5ee040afe8560f6213ae929fa724f73a1238cfcf
Priya M <143039601+priya2075@users.noreply.github.com>
2025-03-15 20:35:51 +0800

    Update ob.js

--- javascripts/ob.js
+++ javascripts/ob.js
@@ -1,3 +1,6 @@
+
+
+
   function rot13(str) {
     return str.replace(/[a-z]/gi, function(c){
       return String.fromCharCode(c.charCodeAt(0) + (c.toLowerCase() < 'n' ? 13 : -13));
@@ -7,14 +10,20 @@
   document.getElementById("email-link").addEventListener("click", function (e) {
     e.preventDefault();
 
-    var encoded = "vnzcevln[qbg]pbagnpg[ng]tznvy[qbg]pbz"; 
-    var decoded = rot13(encoded);
-
     let link = this;
-    this.textContent = "Revealing...";
+
+   
+    if (link.dataset.revealed === "true") {
+      return;
+    }
+
+    link.textContent = "Revealing...";
+    var encoded = "vnzcevln[qbg]pbagnpg[ng]tznvy[qbg]pbz"; 
+    var decoded = rot13(encoded).replaceAll("[qbg]", ".").replaceAll("[ng]", "@");
 
     setTimeout(function() {
-      link.href = "mailto:" + decoded.replaceAll("[qbg]", ".").replaceAll("[ng]", "@");
-      link.textContent = decoded.replaceAll("[qbg]", ".").replaceAll("[ng]", "@");
-    }, 2700); 
+      link.textContent = decoded; 
+      link.dataset.revealed = "true"; 
+    }, 2800); 
   });
+
