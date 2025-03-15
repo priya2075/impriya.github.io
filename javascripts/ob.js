faefc78592b82b3580fa36ba84a706b81d1f7ae0
Priya M <143039601+priya2075@users.noreply.github.com>
2025-03-15 20:37:36 +0800

    Update ob.js

--- javascripts/ob.js
+++ javascripts/ob.js
@@ -1,6 +1,6 @@
 
 
-
+document.addEventListener("DOMContentLoaded", function() {
   function rot13(str) {
     return str.replace(/[a-z]/gi, function(c){
       return String.fromCharCode(c.charCodeAt(0) + (c.toLowerCase() < 'n' ? 13 : -13));
@@ -26,4 +26,4 @@
       link.dataset.revealed = "true"; 
     }, 2800); 
   });
-
+});
